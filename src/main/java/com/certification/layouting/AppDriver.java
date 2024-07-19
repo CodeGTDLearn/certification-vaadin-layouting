7f05bc1400034da675d122f361b4aaeed0268dcc
PauloPortofolio <pvba02@gmail.com>
2024-07-19 10:10:50 -0400

    1.0) Starting Point
    * CONCLUDED:
    - Vaadin Introduction - Module 1/6
    
    * WORKING:
    - Vaadin Layouting - Module 2/6
    
    * NEXT STEPS:
    - Vaadin Creating Forms - Module 3/6
    - Vaadin Datalist with Grid - Module 4/6
    - Vaadin Router API - Module 5/6
    - Vaadin Theming and Styling - Module 6/6
    
    * FUTURE FEATURES:

---
+++ src/main/java/com/certification/layouting/AppDriver.java
@@ -0,0 +1,23 @@
+package com.certification.layouting;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ * <p>
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ */
+@SpringBootApplication
+@Theme(value = "layout")
+public class AppDriver implements AppShellConfigurator {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(AppDriver.class, args);
+  }
+
+}
